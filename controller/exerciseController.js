8b792c128e3fc235723a7e2cb13d176c7f412233
Matias <matiasbenonip@gmail.com>
2024-03-27 09:37:28 -0300

    cambios en la logica de guardado de texto typing-game

--- controller/exerciseController.js
+++ controller/exerciseController.js
@@ -12,13 +12,20 @@ async function getExercises(req, res) {
 async function saveExerciseText(req, res) {
     try {
       const { text } = req.body;
-      const exercise = new Exercise({ text });
+      console.log('Texto recibido:', text); // Verifica si el texto se recibe correctamente
+      let exercise = new Exercise({ text });
+      console.log('Nuevo ejercicio:', exercise); // Verifica el nuevo ejercicio antes de guardarlo
       await exercise.save();
+      console.log('Ejercicio guardado exitosamente'); // Verifica si el ejercicio se guarda correctamente
       res.status(201).json({ message: 'Texto guardado exitosamente' });
     } catch (error) {
+      console.error('Error al guardar el texto:', error);
       res.status(500).json({ error: 'Error al guardar el texto' });
     }
   }
+  
+  
+  
   
 
 module.exports = { getExercises, saveExerciseText };
